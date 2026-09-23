342ca037808d4eada7bf9f2c0259ec7ba0511973
IKVerma <ramanios16256@gmail.com>
2026-09-23 21:01:32 +0530

    feat: types spec

--- src/NativeFastBackgroundRemoval.ts
+++ src/NativeFastBackgroundRemoval.ts
@@ -1,7 +1,7 @@
 import { TurboModuleRegistry, type TurboModule } from 'react-native';
 
 export interface Spec extends TurboModule {
-removeBackground(imageUri: string): Promise<string>;
+  removeBackground(imageUri: string): Promise<string>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('FastBackgroundRemoval');
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import FastBackgroundRemoval from './NativeFastBackgroundRemoval';
 
 export async function removeBackground(imageUri: string): Promise<string> {
-    if (!imageUri || typeof imageUri !== 'string') {
-        throw new Error('[react-native-fast-background-removal] Valid imageUri must be provided.');
-    }
+  if (!imageUri || typeof imageUri !== 'string') {
+    throw new Error(
+      '[react-native-fast-background-removal] Valid imageUri must be provided.'
+    );
+  }
 
-    // File URI वैलिडेशन (file:// या content://)
-    return FastBackgroundRemoval.removeBackground(imageUri);
+  // File URI वैलिडेशन (file:// या content://)
+  return FastBackgroundRemoval.removeBackground(imageUri);
 }
